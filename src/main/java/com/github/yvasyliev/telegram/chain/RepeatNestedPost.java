2055d5d544697382327b67eec1f9c44dbf41e787
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2023-03-27 17:43:47 +0300

    handling nested posts

--- src/main/java/com/github/yvasyliev/telegram/chain/RepeatNestedPost.java
+++ src/main/java/com/github/yvasyliev/telegram/chain/RepeatNestedPost.java
@@ -5,10 +5,16 @@ import com.github.yvasyliev.telegram.TelegramSenderBot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
+import java.util.Properties;
+
 public class RepeatNestedPost extends SubredditPostRepeaterChain {
     @Autowired
     @Qualifier("subredditPostRepeaterChain")
     private SubredditPostRepeaterChain subredditPostRepeaterChain;
+
+    @Autowired
+    @Qualifier("appData")
+    public Properties appData;
 
     public RepeatNestedPost(SubredditPostRepeaterChain nextChain) {
         super(nextChain);
@@ -17,10 +23,14 @@ public class RepeatNestedPost extends SubredditPostRepeaterChain {
     @Override
     public void repeatRedditPost(JsonNode data, TelegramSenderBot telegramSenderBot) {
         if (data.has("crosspost_parent_list")) {
+            var created = appData.getProperty("PREVIOUS_REDDIT_POST_CREATED", "0");
             subredditPostRepeaterChain.repeatRedditPost(
                     data.get("crosspost_parent_list").get(0),
                     telegramSenderBot
             );
+            if (!created.equals(appData.getProperty("PREVIOUS_REDDIT_POST_CREATED"))) {
+                appData.setProperty("PREVIOUS_REDDIT_POST_CREATED", String.valueOf(data.get("created").intValue()));
+            }
         } else {
             super.repeatRedditPost(data, telegramSenderBot);
         }
