521ca1a5ccf501ad7e5b2aebc3f0fe63e35b29a9
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2023-11-04 23:46:55 +0200

    sending gifs as mp4

--- src/main/java/com/github/yvasyliev/service/telegram/posts/GifPostService.java
+++ src/main/java/com/github/yvasyliev/service/telegram/posts/GifPostService.java
@@ -2,18 +2,21 @@ package com.github.yvasyliev.service.telegram.posts;
 
 import com.github.yvasyliev.model.dto.post.GifPost;
 import com.github.yvasyliev.model.dto.post.Post;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendAnimation;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.net.URISyntaxException;
 import java.util.Optional;
 
 @Service(Post.Type.GIF)
 public class GifPostService extends PostService<GifPost, Message> {
     @Override
-    public Optional<Message> applyWithException(String chatId, GifPost post) throws TelegramApiException {
+    @NonNull
+    public Optional<Message> applyWithException(@NonNull String chatId, @NonNull GifPost post) throws TelegramApiException {
         return Optional.ofNullable(sendWithInputStream((inputStream, filename) -> {
             var sendAnimation = SendAnimation.builder()
                     .chatId(chatId)
@@ -24,5 +27,10 @@ public class GifPostService extends PostService<GifPost, Message> {
 
             return redTelBot.execute(sendAnimation);
         }, post));
+    }
+
+    @Override
+    protected String parseFilename(String mediaUrl) throws URISyntaxException {
+        return super.parseFilename(mediaUrl).replace(".gif", ".mp4");
     }
 }
--- src/main/java/com/github/yvasyliev/service/telegram/posts/PostService.java
+++ src/main/java/com/github/yvasyliev/service/telegram/posts/PostService.java
@@ -3,13 +3,16 @@ package com.github.yvasyliev.service.telegram.posts;
 import com.github.yvasyliev.bots.telegram.RedTelBot;
 import com.github.yvasyliev.model.dto.post.MediaPost;
 import com.github.yvasyliev.model.dto.post.Post;
+import org.apache.http.client.utils.URIBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.function.ThrowingBiFunction;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayDeque;
 import java.util.Optional;
 
 public abstract class PostService<U extends Post, R> implements ThrowingBiFunction<String, U, Optional<R>> {
@@ -19,13 +22,14 @@ public abstract class PostService<U extends Post, R> implements ThrowingBiFuncti
     protected Message sendWithInputStream(ThrowingBiFunction<InputStream, String, Message> telegramApiCall, MediaPost post) throws TelegramApiException {
         var mediaUrl = post.getMediaUrl();
         try (var inputStream = new URL(mediaUrl).openStream()) {
-            var filename = mediaUrl.substring(mediaUrl.lastIndexOf('/') + 1);
-            if (filename.contains("?")) {
-                filename = filename.substring(0, filename.indexOf("?"));
-            }
+            var filename = parseFilename(mediaUrl);
             return telegramApiCall.applyWithException(inputStream, filename);
         } catch (Exception e) {
             throw new TelegramApiException(e);
         }
+    }
+
+    protected String parseFilename(String mediaUrl) throws URISyntaxException {
+        return new ArrayDeque<>(new URIBuilder(mediaUrl).getPathSegments()).getLast();
     }
 }
