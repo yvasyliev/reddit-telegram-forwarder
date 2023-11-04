3eb509f92b5d3e380bee817aeff20b67c5c22ee9
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2023-11-04 23:44:42 +0200

    optimized GifPostMapper logic

--- src/main/java/com/github/yvasyliev/service/deserializers/mappers/GifPostMapper.java
+++ src/main/java/com/github/yvasyliev/service/deserializers/mappers/GifPostMapper.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.github.yvasyliev.model.dto.post.GifPost;
 import com.github.yvasyliev.model.dto.post.Post;
 import org.springframework.core.annotation.Order;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -12,19 +13,12 @@ import java.util.Optional;
 @Order(4)
 public class GifPostMapper implements PostMapper {
     @Override
-    public Optional<Post> applyWithException(JsonNode jsonPost) {
+    @NonNull
+    public Optional<Post> applyWithException(@NonNull JsonNode jsonPost) {
         if (isGif(jsonPost)) {
-            var gifUrl = jsonPost
-                    .get("preview")
-                    .get("images")
-                    .get(0)
-                    .get("variants")
-                    .get("mp4")
-                    .get("source")
-                    .get("url")
-                    .textValue();
+            var gifUrl = jsonPost.requiredAt("/preview/images/0/variants/mp4/source/url").textValue();
             var post = new GifPost();
-            post.setText(jsonPost.get("title").textValue());
+            post.setText(jsonPost.path("title").textValue());
             post.setMediaUrl(gifUrl);
             post.setHasSpoiler("nsfw".equals(jsonPost.get("thumbnail").textValue()));
             return Optional.of(post);
@@ -32,9 +26,8 @@ public class GifPostMapper implements PostMapper {
         return Optional.empty();
     }
 
-    private boolean isGif(JsonNode data) {
-        return data.has("url_overridden_by_dest")
-                && data.get("url_overridden_by_dest").textValue().endsWith(".gif")
-                && data.get("preview").get("images").get(0).get("variants").has("mp4");
+    private boolean isGif(JsonNode jsonPost) {
+        return jsonPost.path("url_overridden_by_dest").textValue().endsWith(".gif")
+                && !jsonPost.at("/preview/images/0/variants/mp4").isMissingNode();
     }
 }
