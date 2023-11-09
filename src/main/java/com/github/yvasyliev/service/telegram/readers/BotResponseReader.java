2abd48ed1b9a529a144bcc05315b97f8be623d54
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2023-11-09 10:33:14 +0200

    BotResponseReader -> @Component

--- src/main/java/com/github/yvasyliev/service/telegram/readers/BotResponseReader.java
+++ src/main/java/com/github/yvasyliev/service/telegram/readers/BotResponseReader.java
@@ -1,14 +1,14 @@
 package com.github.yvasyliev.service.telegram.readers;
 
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.util.function.ThrowingFunction;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 
-@Service
+@Component
 public class BotResponseReader implements ThrowingFunction<String, String> {
     @Override
     public String applyWithException(String file) throws URISyntaxException, IOException {
