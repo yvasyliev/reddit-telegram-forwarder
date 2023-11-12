51d37361ed2983211b69bc7d0c8fb71dbc8d7d08
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2023-11-12 21:54:31 +0200

    reading resources as InputStream

--- src/main/java/com/github/yvasyliev/service/telegram/readers/BotResponseReader.java
+++ src/main/java/com/github/yvasyliev/service/telegram/readers/BotResponseReader.java
@@ -5,16 +5,17 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.function.ThrowingFunction;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class BotResponseReader implements ThrowingFunction<String, String> {
     @Override
-    public String applyWithException(String file) throws URISyntaxException, IOException {
-        return Files.readString(new ClassPathResource(file).getFile().toPath())
-                .replace(".", "\\.")
-                .replace("!", "\\!")
-                .replace("-", "\\-");
+    public String applyWithException(String file) throws IOException {
+        try (var inputStream = new ClassPathResource(file).getInputStream()) {
+            return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8)
+                    .replace(".", "\\.")
+                    .replace("!", "\\!")
+                    .replace("-", "\\-");
+        }
     }
 }
