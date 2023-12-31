4b0898697a3535780d3823fc5e2e24dacb6ea5e3
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2023-12-31 13:23:11 +0200

    Revert "log chatId"
    This reverts commit 09b2eb59

--- src/main/java/com/github/yvasyliev/service/factories/ChannelChatFactoryBean.java
+++ src/main/java/com/github/yvasyliev/service/factories/ChannelChatFactoryBean.java
@@ -1,8 +1,6 @@
 package com.github.yvasyliev.service.factories;
 
 import com.github.yvasyliev.bots.telegram.RedditTelegramForwarderBot;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,17 +10,11 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class ChannelChatFactoryBean implements FactoryBean<Chat> {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ChannelChatFactoryBean.class);
     @Autowired
     private RedditTelegramForwarderBot redditTelegramForwarderBot;
 
     @Override
     public Chat getObject() throws TelegramApiException {
-        LOGGER
-                .atDebug()
-                .setMessage("ccid={}")
-                .addArgument(() -> "-1001974198167".equals(redditTelegramForwarderBot.getChannelId()))
-                .log();
         return redditTelegramForwarderBot.execute(new GetChat(redditTelegramForwarderBot.getChannelId()));
     }
 
